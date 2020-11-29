49c7767e8531cce31a63d29bd74431f8bc6ec4dc
Riteesh Reddy Sykam <sykam_r@cs.iitr.ac.in>
2020-11-29 23:49:49 +0530

    Add files via upload

--- code/gui.py
+++ code/gui.py
@@ -8,6 +8,7 @@ import speech_recognition as sr
 from gtts import gTTS
 from playsound import playsound
 import os
+
 pygame.init()
 
 SQUARE_SIDE = 80
@@ -49,16 +50,22 @@ WHITE_BISHOP = pygame.image.load('images/white_bishop.png')
 WHITE_KNIGHT = pygame.image.load('images/white_knight.png')
 WHITE_PAWN = pygame.image.load('images/white_pawn.png')
 WHITE_JOKER = pygame.image.load('images/white_joker.png')
-ONE=pygame.image.load('images/one.png')
-TWO=pygame.image.load('images/two.png')
-THREE=pygame.image.load('images/three.png')
-FOUR=pygame.image.load('images/four.jpg')
-FIVE=pygame.image.load('images/five.jpg')
-SIX=pygame.image.load('images/six.jpg')
-SEVEN=pygame.image.load('images/seven.jpg')
-EIGHT=pygame.image.load('images/eight.jpg')
-
-
+ONE = pygame.image.load('images/one.png')
+TWO = pygame.image.load('images/two.png')
+THREE = pygame.image.load('images/three.png')
+FOUR = pygame.image.load('images/four.jpg')
+FIVE = pygame.image.load('images/five.jpg')
+SIX = pygame.image.load('images/six.jpg')
+SEVEN = pygame.image.load('images/seven.jpg')
+EIGHT = pygame.image.load('images/eight.jpg')
+a = pygame.image.load('images/a.jpg')
+b = pygame.image.load('images/b.jpg')
+c = pygame.image.load('images/c.jpg')
+d = pygame.image.load('images/d.jpg')
+e = pygame.image.load('images/e.jpg')
+f = pygame.image.load('images/f.jpg')
+g = pygame.image.load('images/g.jpg')
+h = pygame.image.load('images/h.jpg')
 
 CLOCK = pygame.time.Clock()
 CLOCK_TICK = 15
@@ -100,7 +107,7 @@ def get_square_rect(square):
 
 
 def coord2str(position, color=chess.WHITE):
-    #print(position[0],position[1])
+    # print(position[0],position[1])
     if color == chess.WHITE:
         file_index = int(position[0] / SQUARE_SIDE)
         rank_index = 7 - int(position[1] / SQUARE_SIDE)
@@ -116,32 +123,47 @@ def print_board(board, color=chess.WHITE):
         printed_board = board
     if color == chess.BLACK:
         printed_board = chess.rotate_board(board)
-    SQUARE_SIDE1=int(SQUARE_SIDE/2)
+    SQUARE_SIDE1 = int(SQUARE_SIDE / 2)
     print_empty_board()
-    SCREEN.blit(pygame.transform.scale(ONE, (SQUARE_SIDE1, SQUARE_SIDE1)),pygame.Rect(( 8*SQUARE_SIDE, int(0.25* SQUARE_SIDE)), (SQUARE_SIDE1, SQUARE_SIDE1)))
-    SCREEN.blit(pygame.transform.scale(TWO, (SQUARE_SIDE1, SQUARE_SIDE1)),pygame.Rect(( 8*SQUARE_SIDE, int(1.25* SQUARE_SIDE)), (SQUARE_SIDE1, SQUARE_SIDE1)))
-    SCREEN.blit(pygame.transform.scale(THREE, (SQUARE_SIDE1, SQUARE_SIDE1)),pygame.Rect(( 8*SQUARE_SIDE,int(2.25* SQUARE_SIDE)), (SQUARE_SIDE1, SQUARE_SIDE1)))
-    SCREEN.blit(pygame.transform.scale(FOUR, (SQUARE_SIDE1, SQUARE_SIDE1)),pygame.Rect(( 8*SQUARE_SIDE, int(3.25* SQUARE_SIDE)), (SQUARE_SIDE1, SQUARE_SIDE1)))
-    SCREEN.blit(pygame.transform.scale(FIVE, (SQUARE_SIDE1, SQUARE_SIDE1)),pygame.Rect(( 8*SQUARE_SIDE, int(4.25* SQUARE_SIDE)), (SQUARE_SIDE1, SQUARE_SIDE1)))
-    SCREEN.blit(pygame.transform.scale(SIX, (SQUARE_SIDE1, SQUARE_SIDE1)),pygame.Rect(( 8*SQUARE_SIDE, int(5.25* SQUARE_SIDE)), (SQUARE_SIDE1, SQUARE_SIDE1)))
-    SCREEN.blit(pygame.transform.scale(SEVEN, (SQUARE_SIDE1, SQUARE_SIDE1)),pygame.Rect(( 8*SQUARE_SIDE,int(6.25* SQUARE_SIDE)), (SQUARE_SIDE1, SQUARE_SIDE1)))
-    SCREEN.blit(pygame.transform.scale(EIGHT, (SQUARE_SIDE1, SQUARE_SIDE1)),pygame.Rect(( 8*SQUARE_SIDE, int(7.25* SQUARE_SIDE)), (SQUARE_SIDE1, SQUARE_SIDE1)))
-    SCREEN.blit(pygame.transform.scale(ONE, (SQUARE_SIDE1, SQUARE_SIDE1)),pygame.Rect(( int(0.25*SQUARE_SIDE), 8* SQUARE_SIDE), (SQUARE_SIDE1, SQUARE_SIDE1)))
-    SCREEN.blit(pygame.transform.scale(TWO, (SQUARE_SIDE1, SQUARE_SIDE1)),pygame.Rect(( int(1.25*SQUARE_SIDE), 8* SQUARE_SIDE), (SQUARE_SIDE1, SQUARE_SIDE1)))
-    SCREEN.blit(pygame.transform.scale(THREE, (SQUARE_SIDE1, SQUARE_SIDE1)),pygame.Rect((int(2.25*SQUARE_SIDE), 8* SQUARE_SIDE), (SQUARE_SIDE1, SQUARE_SIDE1)))
-    SCREEN.blit(pygame.transform.scale(FOUR, (SQUARE_SIDE1, SQUARE_SIDE1)),pygame.Rect(( int(3.25*SQUARE_SIDE), 8* SQUARE_SIDE), (SQUARE_SIDE1, SQUARE_SIDE1)))
-    SCREEN.blit(pygame.transform.scale(FIVE, (SQUARE_SIDE1, SQUARE_SIDE1)),pygame.Rect(( int(4.25*SQUARE_SIDE), 8* SQUARE_SIDE), (SQUARE_SIDE1, SQUARE_SIDE1)))
-    SCREEN.blit(pygame.transform.scale(SIX, (SQUARE_SIDE1, SQUARE_SIDE1)),pygame.Rect(( int(5.25*SQUARE_SIDE), 8* SQUARE_SIDE), (SQUARE_SIDE1, SQUARE_SIDE1)))
-    SCREEN.blit(pygame.transform.scale(SEVEN, (SQUARE_SIDE1, SQUARE_SIDE1)),pygame.Rect((int(6.25*SQUARE_SIDE), 8* SQUARE_SIDE), (SQUARE_SIDE1, SQUARE_SIDE1)))
-    SCREEN.blit(pygame.transform.scale(EIGHT, (SQUARE_SIDE1, SQUARE_SIDE1)),pygame.Rect(( int(7.25*SQUARE_SIDE), 8* SQUARE_SIDE), (SQUARE_SIDE1, SQUARE_SIDE1)))
-    
+    SCREEN.blit(pygame.transform.scale(ONE, (SQUARE_SIDE1, SQUARE_SIDE1)),
+                pygame.Rect((8 * SQUARE_SIDE, int(0.25 * SQUARE_SIDE)), (SQUARE_SIDE1, SQUARE_SIDE1)))
+    SCREEN.blit(pygame.transform.scale(TWO, (SQUARE_SIDE1, SQUARE_SIDE1)),
+                pygame.Rect((8 * SQUARE_SIDE, int(1.25 * SQUARE_SIDE)), (SQUARE_SIDE1, SQUARE_SIDE1)))
+    SCREEN.blit(pygame.transform.scale(THREE, (SQUARE_SIDE1, SQUARE_SIDE1)),
+                pygame.Rect((8 * SQUARE_SIDE, int(2.25 * SQUARE_SIDE)), (SQUARE_SIDE1, SQUARE_SIDE1)))
+    SCREEN.blit(pygame.transform.scale(FOUR, (SQUARE_SIDE1, SQUARE_SIDE1)),
+                pygame.Rect((8 * SQUARE_SIDE, int(3.25 * SQUARE_SIDE)), (SQUARE_SIDE1, SQUARE_SIDE1)))
+    SCREEN.blit(pygame.transform.scale(FIVE, (SQUARE_SIDE1, SQUARE_SIDE1)),
+                pygame.Rect((8 * SQUARE_SIDE, int(4.25 * SQUARE_SIDE)), (SQUARE_SIDE1, SQUARE_SIDE1)))
+    SCREEN.blit(pygame.transform.scale(SIX, (SQUARE_SIDE1, SQUARE_SIDE1)),
+                pygame.Rect((8 * SQUARE_SIDE, int(5.25 * SQUARE_SIDE)), (SQUARE_SIDE1, SQUARE_SIDE1)))
+    SCREEN.blit(pygame.transform.scale(SEVEN, (SQUARE_SIDE1, SQUARE_SIDE1)),
+                pygame.Rect((8 * SQUARE_SIDE, int(6.25 * SQUARE_SIDE)), (SQUARE_SIDE1, SQUARE_SIDE1)))
+    SCREEN.blit(pygame.transform.scale(EIGHT, (SQUARE_SIDE1, SQUARE_SIDE1)),
+                pygame.Rect((8 * SQUARE_SIDE, int(7.25 * SQUARE_SIDE)), (SQUARE_SIDE1, SQUARE_SIDE1)))
+    SCREEN.blit(pygame.transform.scale(a, (SQUARE_SIDE1, SQUARE_SIDE1)),
+                pygame.Rect((int(0.25 * SQUARE_SIDE), 8 * SQUARE_SIDE), (SQUARE_SIDE1, SQUARE_SIDE1)))
+    SCREEN.blit(pygame.transform.scale(b, (SQUARE_SIDE1, SQUARE_SIDE1)),
+                pygame.Rect((int(1.25 * SQUARE_SIDE), 8 * SQUARE_SIDE), (SQUARE_SIDE1, SQUARE_SIDE1)))
+    SCREEN.blit(pygame.transform.scale(c, (SQUARE_SIDE1, SQUARE_SIDE1)),
+                pygame.Rect((int(2.25 * SQUARE_SIDE), 8 * SQUARE_SIDE), (SQUARE_SIDE1, SQUARE_SIDE1)))
+    SCREEN.blit(pygame.transform.scale(d, (SQUARE_SIDE1, SQUARE_SIDE1)),
+                pygame.Rect((int(3.25 * SQUARE_SIDE), 8 * SQUARE_SIDE), (SQUARE_SIDE1, SQUARE_SIDE1)))
+    SCREEN.blit(pygame.transform.scale(e, (SQUARE_SIDE1, SQUARE_SIDE1)),
+                pygame.Rect((int(4.25 * SQUARE_SIDE), 8 * SQUARE_SIDE), (SQUARE_SIDE1, SQUARE_SIDE1)))
+    SCREEN.blit(pygame.transform.scale(f, (SQUARE_SIDE1, SQUARE_SIDE1)),
+                pygame.Rect((int(5.25 * SQUARE_SIDE), 8 * SQUARE_SIDE), (SQUARE_SIDE1, SQUARE_SIDE1)))
+    SCREEN.blit(pygame.transform.scale(g, (SQUARE_SIDE1, SQUARE_SIDE1)),
+                pygame.Rect((int(6.25 * SQUARE_SIDE), 8 * SQUARE_SIDE), (SQUARE_SIDE1, SQUARE_SIDE1)))
+    SCREEN.blit(pygame.transform.scale(h, (SQUARE_SIDE1, SQUARE_SIDE1)),
+                pygame.Rect((int(7.25 * SQUARE_SIDE), 8 * SQUARE_SIDE), (SQUARE_SIDE1, SQUARE_SIDE1)))
+
     if chess.is_check(board, chess.WHITE):
         paint_square(chess.bb2str(chess.get_king(printed_board, chess.WHITE)), RED_CHECK)
     if chess.is_check(board, chess.BLACK):
         paint_square(chess.bb2str(chess.get_king(printed_board, chess.BLACK)), RED_CHECK)
 
     for position in chess.colored_piece_gen(printed_board, chess.KING, chess.BLACK):
-        
         SCREEN.blit(pygame.transform.scale(BLACK_KING, (SQUARE_SIDE, SQUARE_SIDE)),
                     get_square_rect(chess.bb2str(position)))
     for position in chess.colored_piece_gen(printed_board, chess.QUEEN, chess.BLACK):
@@ -232,9 +254,9 @@ def play_as(game, color):
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     run = False
-                myText1="make your move"
+                myText1 = "make your move"
                 if event.type == pygame.MOUSEBUTTONDOWN:
-                    
+
                     r = sr.Recognizer()
                     print("Please Talk")
                     language = 'en'
@@ -245,74 +267,77 @@ def play_as(game, color):
                     playsound("Output.mp3")
                     os.remove("Output.mp3")
                     with sr.Microphone() as source:
-                        
-                        
                         try:
-                            
                             audio_data = r.record(source, duration=5)
-                        
                             print("Recognising...")
                             text = r.recognize_google(audio_data)
-                            text=text.replace(" ","")
+                            text = text.replace(" ", "")
                             print(text)
                         except sr.UnknownValueError:
                             language = 'en'
-
                             output = gTTS(text="I didn't get that. Say again", lang=language, slow=False)
-
                             output.save("Output.mp3")
                             playsound("Output.mp3")
                             os.remove("Output.mp3")
                             print("I didn't get that. Say again")
                             continue
-                    
-                    l=['1','2','3','4','5','6','7','8']
-                    flag=True
-                    for _ in text:
-                        if _ not in l:
-                            flag=False
-                            break
-                    if flag==True:
-                        leaving_square = coord2str(((int(text[0])-1)*SQUARE_SIDE,(int(text[1])-1)*SQUARE_SIDE), color)
-                        arriving_square = coord2str(((int(text[-2])-1)*SQUARE_SIDE,(int(text[-1])-1)*SQUARE_SIDE), color)
-                    
+                    l = ['1', '2', '3', '4', '5', '6', '7', '8']
+                    k = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+                    flag = True
+                    if len(text)!=4:
+                        flag = False
 
-                
-                    myText="Please make a valid move"
+                    if text[1] not in l:
+                        flag = False
+                    if text[-1] not in l:
+                        flag = False
+
+                    text = text.replace("A", "1")
+                    text = text.replace("a", "1")
+                    text = text.replace("B", "2")
+                    text = text.replace("b", "2")
+                    text = text.replace("C", "3")
+                    text = text.replace("c", "3")
+                    text = text.replace("D", "4")
+                    text = text.replace("d", "4")
+                    text = text.replace("E", "5")
+                    text = text.replace("e", "5")
+                    text = text.replace("F", "6")
+                    text = text.replace("f", "6")
+                    text = text.replace("G", "7")
+                    text = text.replace("g", "7")
+                    text = text.replace("H", "8")
+                    text = text.replace("h", "8")
+
+
+                    if flag:
+                        leaving_square = coord2str(((int(text[0]) - 1) * SQUARE_SIDE, (int(text[1]) - 1) * SQUARE_SIDE),color)
+                        arriving_square = coord2str(((int(text[-2]) - 1) * SQUARE_SIDE, (int(text[-1]) - 1) * SQUARE_SIDE), color)
+
+                    myText = "Please make a valid move"
                     if ongoing and game.to_move == color:
-                        if flag==True and len(text)==4:
+                        if flag == True and len(text) == 4:
                             move = (chess.str2bb(leaving_square), chess.str2bb(arriving_square))
                             if move not in chess.legal_moves(game, game.to_move):
                                 language = 'en'
-
                                 output = gTTS(text=myText, lang=language, slow=False)
-
                                 output.save("Output.mp3")
                                 playsound("Output.mp3")
                                 os.remove("Output.mp3")
-
-                                
                                 print("Please make a valid move")
-                                
                             else:
                                 game = try_move(game, move)
                                 print_board(game.board, color)
-                                
-                            
-                        else :
-                            
+
+
+                        else:
                             language = 'en'
-
                             output = gTTS(text=myText, lang=language, slow=False)
-
                             output.save("Output.mp3")
                             playsound("Output.mp3")
                             os.remove("Output.mp3")
-                            
-                            #print_board(game.board, color)
+                            # print_board(game.board, color)
                             print("Please make a valid move")
-                            
-                        
 
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_ESCAPE or event.key == 113:
@@ -331,9 +356,9 @@ def play_as(game, color):
                         new_colors.remove(BOARD_COLOR)
                         BOARD_COLOR = choice(new_colors)
                         print_board(game.board, color)
-                    if event.key==114:
+                    if event.key == 114:
                         os.system("start Rulebook.pdf")
-                        
+
                     if event.key == 112 or event.key == 100:  # P or D key
                         print(game.get_move_list() + '\n')
                         print('\n'.join(game.position_history))
